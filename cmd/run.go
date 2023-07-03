4a3c370bda0d912fdc05f6dbbc0f4fa3e70a28ab
DESKTOP-5P0IV89 <DESKTOP-5P0IV89@emxple.com>
2023-07-03 13:16:10 +0800

    first commit

--- cmd/run.go
+++ cmd/run.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api/v1api"
 	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/cors/wrapper/gin"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
+	"net/http"
 )
 
 var Run = &cli.Command{
@@ -19,36 +24,53 @@ var Run = &cli.Command{
 		defer ncloser()
 		ctx := lcli.ReqContext(ctxx)
 
-		//	 创建一个区块同步监听器
-		listener := make(chan []*api.HeadChange)
-
-		// 启动监听器
-		go func() {
-			for changes := range listener {
-				for _, change := range changes {
-					for _, block := range change.Val.Blocks() {
-						fmt.Println("收到区块:", block.Cid().String(), "bk ", block.Miner.String())
-					}
-				}
-			}
-		}()
-		// 获取ChainSync API
-		sub, err := chainAPI.ChainNotify(ctx)
+		err = StartBlock(ctx, chainAPI)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		// 开始监听区块同步事件
-		go func() {
-			for {
-				select {
-				case changes := <-sub:
-					listener <- changes
-				case <-ctx.Done():
-					return
+
+		r := gin.Default()
+		r.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		r.Run()
+
+		return nil
+	},
+}
+
+func StartBlock(ctx context.Context, chainAPI v1api.FullNode) error {
+
+	//	 创建一个区块同步监听器
+	listener := make(chan []*api.HeadChange)
+
+	// 启动监听器
+	go func() {
+		for changes := range listener {
+			for _, change := range changes {
+				for _, block := range change.Val.Blocks() {
+					fmt.Println("收到区块:", block.Cid().String(), "bk ", block.Miner.String())
 				}
 			}
-		}()
-		return nil
-
-	},
+		}
+	}()
+	// 获取ChainSync API
+	sub, err := chainAPI.ChainNotify(ctx)
+	if err != nil {
+		panic(err)
+	}
+	// 开始监听区块同步事件
+	go func() {
+		for {
+			select {
+			case changes := <-sub:
+				listener <- changes
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,22 +2,5 @@ module github.com/switfs/switfs-block
 
 go 1.20
 
-require (
-	github.com/filecoin-project/lotus v1.23.2
-	github.com/ipfs/go-log/v2 v2.5.1
-	github.com/urfave/cli v1.22.10
-	github.com/urfave/cli/v2 v2.16.3
-)
 replace github.com/filecoin-project/filecoin-ffi => ./extern/filecoin-ffi
 
-require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/mattn/go-isatty v0.0.18 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/goleak v1.1.12 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-)
