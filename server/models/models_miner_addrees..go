dc097612bb29b90c3b4fc6a2a81a64f5bc332af5
DESKTOP-5P0IV89 <DESKTOP-5P0IV89@emxple.com>
2023-07-14 14:56:04 +0800

    ssssss

--- server/models/models_miner_addrees..go
+++ server/models/models_miner_addrees..go
@@ -1,12 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Miner struct {
-	ID           uint      `gorm:"primarykey"`
+	ID           uint      `gorm:"primary"`
 	MinerAddress string    `gorm:"miner_address" json:"miner_address"`
 	MinerCreate  time.Time `gorm:"miner_create" json:"miner_create"`
 	MinerUpdate  time.Time `gorm:"miner_update" json:"miner_update"`
+	IsDelete     int       `gorm:"is_delete" json:"is_delete"`
 }
 
 func (Miner) TableName() string {
